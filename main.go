ebe7ab4595a9514fdfa2c1c04a28c171462de5e3
krilo <krilo_c@hotmail.com>
2021-09-11 11:29:11 +0800

    recipe data model

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"time"
 )
+
+type Recipe struct{
+	Name string `json:"name"`
+	Tags []string `json:"tags"`
+	Ingredients []string `json:"ingredients"`
+	Instructions []string `json:"instructions"`
+	PublishedAt time.Time `json:"published_at"`
+}
 
 func main() {
 	r := gin.Default()
