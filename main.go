3c11736642407dd656d769ba1980a0653c17a194
chengkrilo <krilo_c@hotmail>
2021-09-11 14:24:25 +0800

    list recipes endpoint

--- main.go
+++ main.go
@@ -2,8 +2,10 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -21,6 +23,8 @@ var recipes []Recipe
 
 func init(){
 	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
 }
 
 func NewRecipeHandler(c *gin.Context){
@@ -36,12 +40,17 @@ func NewRecipeHandler(c *gin.Context){
 	c.JSON(http.StatusOK, recipe)
 }
 
+func ListRecipesHandler(c *gin.Context){
+	c.JSON(http.StatusOK, recipes)
+}
+
 func main() {
 	router := gin.Default()
 	//r.GET("/hello", func(c *gin.Context) {
 	//	c.String(http.StatusOK, "OK")
 	//})
 	router.POST("/recipes",NewRecipeHandler)
+	router.GET("/recipes", ListRecipesHandler)
 	router.Run()
 
 }
