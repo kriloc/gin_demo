b29f3d021d68d9a003308500bc0a8724211c02a3
chengkrilo <krilo_c@hotmail>
2021-09-27 15:33:29 +0800

    delete recipe endpoint

--- main.go
+++ main.go
@@ -75,6 +75,30 @@ func main() {
 	router.POST("/recipes",NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("recipes/:id", DeleteRecipeHandler)
 	router.Run()
 
+}
+
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1  // 初始化，不能為0，所以是-1
+	for i:=0 ; i<len(recipes); i++{
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {  // 表示找不到
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found."})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...) // 將此index之前跟之後加回去
+	c.JSON(http.StatusOK, gin.H{
+		"message":"Recipe has been deleted."})
+
+	// test
+	//curl -v -sX DELETE http://localhost:8080/recipes/c0283p3d0cvuglq85log
 }
