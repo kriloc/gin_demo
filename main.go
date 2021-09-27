99476b3c24155b368be8fc70b3f15e22fb32291b
chengkrilo <krilo_c@hotmail>
2021-09-27 18:57:01 +0800

    search recipe by tag

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/rs/xid"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -76,6 +77,7 @@ func main() {
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.DELETE("recipes/:id", DeleteRecipeHandler)
+	router.GET("/recipes/search", SearchRecipesHandler)
 	router.Run()
 
 }
@@ -101,4 +103,23 @@ func DeleteRecipeHandler(c *gin.Context) {
 
 	// test
 	//curl -v -sX DELETE http://localhost:8080/recipes/c0283p3d0cvuglq85log
+}
+
+func SearchRecipesHandler(c *gin.Context) {
+	tag := c.Query("tag")
+	listOfRecipes := make([]Recipe, 0)
+
+	for i := 0; i < len(recipes); i++ {
+		found := false
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag){
+				found = true
+			}
+		}
+		if found {
+			listOfRecipes = append(listOfRecipes, recipes[i])
+		}
+	}
+	c.JSON(http.StatusOK, listOfRecipes)
+	//http://localhost:8080/recipes/search?tag=italian
 }
